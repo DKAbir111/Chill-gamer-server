2f5ac21bde2e4fca859dd65ffacefdb7e1369a6f
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-12-05 16:01:08 +0600

    find one game review based on id

--- routes/game.route.js
+++ routes/game.route.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const { ObjectId } = require('mongodb');
 const router = express.Router()
 const createGameRoute = (gameCollection) => {
     router.post('/games', async (req, res) => {
@@ -6,6 +7,19 @@ const createGameRoute = (gameCollection) => {
         const result = await gameCollection.insertOne(newData);
         res.send(result);
     })
+
+    router.get('/reviews', async (req, res) => {
+        const games = await gameCollection.find().toArray();
+        res.send(games);
+    })
+
+    router.get('/review/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const data = await gameCollection.findOne(query);
+        res.send(data);
+    })
+
     return router;
 }
 
